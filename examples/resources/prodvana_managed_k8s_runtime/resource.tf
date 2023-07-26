f90600e545d6c5e31cc903890406c0eef48cce6b
Mike Cugini <mike@prodvana.io>
2023-07-26 13:55:56 -0400

    update examples and make them more friendly to pulumi tf bridging (#20)

--- examples/resources/prodvana_managed_k8s_runtime/resource.tf
+++ examples/resources/prodvana_managed_k8s_runtime/resource.tf
@@ -1,19 +1,9 @@
-resource "google_container_cluster" "cluster" {
-  name     = "my-gke-cluster"
-  location = "us-central1"
-
-  // <...configuration elided...>
-}
-
-data "google_client_config" "default" {}
-
 resource "prodvana_managed_k8s_runtime" "example" {
   name = "my-k8s-runtime"
   agent_env = {
     "PROXY" = "http://localhost:8080"
   }
 
-  host                   = google_container_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.test.master_auth.0.cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
+  config_path    = "~/.kube/config"
+  config_context = "my-k8s-context"
 }
