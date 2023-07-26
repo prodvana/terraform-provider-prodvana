a1dbef1882d4ffe8c4706931e020a94302671bbe
Mike Cugini <mike@prodvana.io>
2023-07-26 14:51:29 +0000

    update example

--- examples/resources/prodvana_managed_k8s_runtime/resource.tf
+++ examples/resources/prodvana_managed_k8s_runtime/resource.tf
@@ -1,35 +1,11 @@
-// EKS Example
-
-resource "aws_eks_cluster" "cluster" {
-  name = "my-eks-cluster"
-
-  // <...configuration elided...>
-}
-
-// Note: this requires the AWS provider be configured with IAM credentials that have access to the EKS cluster
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.cluster.name
-}
-
-resource "prodvana_managed_k8s_runtime" "example" {
-  name = "my-k8s-runtime"
-  agent_env = {
-    "PROXY" = "http://localhost:8080"
-  }
-
-  host                   = data.aws_eks_cluster_auth.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster_auth.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-// GKE Example
-
 resource "google_container_cluster" "cluster" {
   name     = "my-gke-cluster"
   location = "us-central1"
 
   // <...configuration elided...>
 }
+
+data "google_client_config" "default" {}
 
 resource "prodvana_managed_k8s_runtime" "example" {
   name = "my-k8s-runtime"
@@ -38,7 +14,6 @@ resource "prodvana_managed_k8s_runtime" "example" {
   }
 
   host                   = google_container_cluster.cluster.endpoint
-  cluster_ca_certificate = google_container_cluster.cluster.master_auth.0.cluster_ca_certificate
-  client_certificate     = google_container_cluster.cluster.master_auth.0.client_certificate
-  client_key             = google_container_cluster.cluster.master_auth.0.client_key
+  cluster_ca_certificate = base64decode(google_container_cluster.test.master_auth.0.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
 }
